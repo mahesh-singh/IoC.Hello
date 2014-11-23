05f7e2d3374252b1cf4bcb13fc7e70a02f49d37b
Mahesh Singh <singh.mahesh@gmail.com>
2014-11-23 22:59:21 +0530

    Removed the DateTime.Now dependency by adding IClock Interface

--- IoC.App/Autocue.cs
+++ IoC.App/Autocue.cs
@@ -7,15 +7,19 @@ namespace IoC.App
 {
     public class Autocue
     {
+        private IClock _clock;
+
         public string GetSomethingIntelligent()
         {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
+            DateTime now = _clock.Now;
+
+            if (now.DayOfWeek == DayOfWeek.Thursday)
             {
-                if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 20)
+                if (now.Hour >= 18 && DateTime.Now.Hour < 20)
                 {
                     return "Good evening!";
                 }
-                else if (DateTime.Now.Hour >= 20)
+                else if (now.Hour >= 20)
                 {
                     return "Get out of the way!";
                 }
