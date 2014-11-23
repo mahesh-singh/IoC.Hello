fa62e7758097ad681b029bb50bdd6d829947abeb
Mahesh Singh <singh.mahesh@gmail.com>
2014-11-23 22:29:28 +0530

    initial commit

---
+++ IoC.App/Clarkson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IoC.App
+{
+    class Clarkson
+    {
+        internal void ShoutSomthing()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Thursday)
+            {
+                if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 20)
+                {
+                    Console.WriteLine("Good evening!");
+                }
+                else if (DateTime.Now.Hour >= 20)
+                {
+                    Console.WriteLine("Get out of the way!"); 
+                }
+            }
+            else 
+            {
+                Console.WriteLine("You're wrong!");
+            }
+
+        }
+    }
+}
---
+++ IoC.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clarkson = new Clarkson();
+
+            clarkson.ShoutSomthing();
+
+
+            Console.ReadKey();
+        }
+    }
+}
